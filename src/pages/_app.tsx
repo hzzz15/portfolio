dc7329fae50bb25ef94132dc51268174b181cd09
강희지 <shine_g_0517@naver.com>
2025-03-11 17:17:12 +0900

    틀끗

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,10 +2,13 @@ import "@/styles/globals.css";
 import { DefaultSeo } from "next-seo";
 import { ThemeProvider } from "next-themes";
 import type { AppProps } from "next/app";
+import * as gtag from "../lib/gtag";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+import Script from "next/script";
 
-/**
- * @description SEO를 위해 본인의 정보로 수정해주세요.
- */
+const GA_TRACKING_ID = process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS;
+
 const DEFAULT_SEO = {
   title: "강희지 | ML Engineer",
   description: "안녕하세요, 머신러닝 개발자를 꿈꾸는 강희지 입니다",
@@ -13,7 +16,7 @@ const DEFAULT_SEO = {
   openGraph: {
     type: "website",
     locale: "ko_KR",
-    url: "https://www.naver.com/", 
+    url: "https://www.naver.com/",
     title: "강희지 | ML Engineer",
     site_name: "강희지 | ML Engineer",
     images: [
@@ -48,8 +51,41 @@ const DEFAULT_SEO = {
 };
 
 const App = ({ Component, pageProps }: AppProps) => {
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = (url: URL) => {
+      gtag.pageview(url);
+    };
+    router.events.on("routeChangeComplete", handleRouteChange);
+    router.events.on("hashChangeComplete", handleRouteChange);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+      router.events.off("hashChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
+
   return (
     <>
+      {/* Global Site Tag (gtag.js) - Google Analytics */}
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+      />
+      <Script
+        id="gtag-init"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
+        }}
+      />
       <DefaultSeo {...DEFAULT_SEO} />
       <ThemeProvider attribute="class">
         <Component {...pageProps} />
